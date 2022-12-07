758499e98a082ae9b046a558eee6ceb93b680e8a
LevkovichKirill <levkovich.kirill.1@mail.ru>
2022-12-07 18:32:32 +0300

    add UI to search VC

---
+++ GitHubFollowers/FavoritesListVC.swift
@@ -0,0 +1,29 @@
+//
+//  FavoritesListVC.swift
+//  GitHubFollowers
+//
+//  Created by Ya on 7.12.22.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GitHubFollowers/GFTextField.swift
@@ -0,0 +1,20 @@
+//
+//  GFTextField.swift
+//  GitHubFollowers
+//
+//  Created by Ya on 7.12.22.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
