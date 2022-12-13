d4ec5f88d4681c71f62a73d3408286615bff3993
LevkovichKirill <levkovich.kirill.1@mail.ru>
2022-12-13 17:48:29 +0300

    add model

---
+++ GitHubFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitHubFollowers
+//
+//  Created by Ya on 13.12.22.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
