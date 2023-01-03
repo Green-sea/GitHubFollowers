072e4762e2513402d10c229b4494fd00ceef00f6
LevkovichKirill <levkovich.kirill.1@mail.ru>
2023-01-03 13:13:49 +0300

    add URLSession for userInfo VC

--- GitHubFollowers/Screens/UserInfoVC.swift
+++ GitHubFollowers/Screens/UserInfoVC.swift
@@ -17,8 +17,21 @@ class UserInfoVC: UIViewController {
         
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done,
                                          target: self, action: #selector(dissmissVC))
-        print(userName)
+        print(userName!)
         navigationItem.rightBarButtonItem = doneButton
+        
+        NetworkManager.shared.getUserInfo(for: userName) { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(let user):
+                user
+                
+            case .failure(let error):
+                self.present(GFAlertVC(title: "Something went wrong",
+                                       message: error.rawValue, buttonTitle: "ok"), animated: true)
+            }
+        }
     }
     
     
