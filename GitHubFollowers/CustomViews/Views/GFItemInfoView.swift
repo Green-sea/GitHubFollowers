2fa2d7034a7e8fdc08a429840dc6cc530bcad203
LevkovichKirill <levkovich.kirill.1@mail.ru>
2023-01-24 15:05:36 +0300

    add itemInfoView

---
+++ GitHubFollowers/CustomViews/Views/GFItemInfoView.swift
@@ -0,0 +1,20 @@
+//
+//  GFItemInfoView.swift
+//  GitHubFollowers
+//
+//  Created by Ya on 24.01.23.
+//
+
+import UIKit
+
+class GFItemInfoView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
